510d71dff7482c9b090d5ffc5cd07d36d5140270
Awesohame <82087247+awesohame@users.noreply.github.com>
2025-05-22 22:38:57 +0530

    after autoformatting setup

--- template.cpp
+++ template.cpp
@@ -43,8 +43,8 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 
 // Ordered Set
-template <typename T,typename Compare=less<T>>
-using ordered_set=__gnu_pbds::tree<
+template <typename T, typename Compare = less<T>>
+using ordered_set = __gnu_pbds::tree<
     T,
     __gnu_pbds::null_type,
     Compare,
@@ -53,90 +53,90 @@ using ordered_set=__gnu_pbds::tree<
 >;
 
 // custom map
-struct chash{ static uint64_t splitmix64(uint64_t x){ x+=0x9e3779b97f4a7c15;x=(x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x=(x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31); }size_t operator()(uint64_t x) const{ static const uint64_t FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM); } };
-struct pair_hash{ static uint64_t splitmix64(uint64_t x){ x+=0x9e3779b97f4a7c15;x=(x^(x>>30))*0xbf58476d1ce4e5b9;x=(x^(x>>27))*0x94d049bb133111eb;return x^(x>>31); }size_t operator()(pii x)const{ static const uint64_t FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first+FIXED_RANDOM)^(splitmix64(x.second+FIXED_RANDOM)>>1); } };
+struct chash { static uint64_t splitmix64(uint64_t x) { x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31); }size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM); } };
+struct pair_hash { static uint64_t splitmix64(uint64_t x) { x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31); }size_t operator()(pii x)const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x.first + FIXED_RANDOM) ^ (splitmix64(x.second + FIXED_RANDOM) >> 1); } };
 
-template<typename T1,typename T2>
-using safe_map=unordered_map<T1,T2,chash>;
+template<typename T1, typename T2>
+using safe_map = unordered_map<T1, T2, chash>;
 
 // input and output
 template <typename T>
-istream& operator>>(istream& in,vector<T>& a){ for(auto& x : a) in >> x;return in; }
+istream& operator>>(istream& in, vector<T>& a) { for(auto& x : a) in >> x; return in; }
 
 template <typename T>
-ostream& operator<<(ostream& out,const vector<T>& a){ for(const auto& x : a) out << x << " ";return out; }
+ostream& operator<<(ostream& out, const vector<T>& a) { for(const auto& x : a) out << x << " "; return out; }
 
 template <typename T>
-istream& operator>>(istream& in,vector<vector<T>>& a){ for(auto& row : a) in >> row;return in; }
+istream& operator>>(istream& in, vector<vector<T>>& a) { for(auto& row : a) in >> row; return in; }
 
 template <typename T>
-ostream& operator<<(ostream& out,const vector<vector<T>>& a){ for(const auto& row : a) out << row << endl;return out; }
+ostream& operator<<(ostream& out, const vector<vector<T>>& a) { for(const auto& row : a) out << row << endl; return out; }
 
-template <typename T1,typename T2>
-istream& operator>>(istream& in,vector<pair<T1,T2>>& a){ for(auto& x : a) in >> x.first >> x.second;return in; }
+template <typename T1, typename T2>
+istream& operator>>(istream& in, vector<pair<T1, T2>>& a) { for(auto& x : a) in >> x.first >> x.second; return in; }
 
-template <typename T1,typename T2>
-ostream& operator<<(ostream& out,const vector<pair<T1,T2>>& a){ for(const auto& x : a) out << x.first << " " << x.second << endl;return out; }
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const vector<pair<T1, T2>>& a) { for(const auto& x : a) out << x.first << " " << x.second << endl; return out; }
 
 template <typename T>
-void pl(const T& x){
-    cout<<x<<endl;
+void pl(const T& x) {
+    cout << x << endl;
 }
 
 // arrays
 template <typename T> // get indices all occurrences of an element
-vector<int> allOccur(const vector<T>& a,T e){ vector<int> indices;for(int i=0; i < a.size(); ++i){ if(a[i] == e) indices.push_back(i); }return indices; }
+vector<int> allOccur(const vector<T>& a, T e) { vector<int> indices; for(int i = 0; i < a.size(); ++i) { if(a[i] == e) indices.push_back(i); }return indices; }
 
 template <typename T> // gcd of array
-T gcdArr(vector<T>& a){ return accumulate(a.begin() + 1,a.end(),a[0],[](T x,T y){return gcd(x,y);}); }
+T gcdArr(vector<T>& a) { return accumulate(a.begin() + 1, a.end(), a[0], [](T x, T y) {return gcd(x, y); }); }
 
 // functions
-ll min(ll a,int b){ if(a < b) return a; return b; }
-ll min(int a,ll b){ if(a < b) return a; return b; }
-ll max(ll a,int b){ if(a > b) return a; return b; }
-ll max(int a,ll b){ if(a > b) return a; return b; }
-ll gcd(ll a,ll b){ if(b == 0) return a; return gcd(b,a % b); }
-ll lcm(ll a,ll b){ return a / gcd(a,b) * b; }
+ll min(ll a, int b) { if(a < b) return a; return b; }
+ll min(int a, ll b) { if(a < b) return a; return b; }
+ll max(ll a, int b) { if(a > b) return a; return b; }
+ll max(int a, ll b) { if(a > b) return a; return b; }
+ll gcd(ll a, ll b) { if(b == 0) return a; return gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 
 // utils
 #define yes printYes()
 #define no printNo()
-string to_upper(string a){ for(int i=0; i < (int)a.size(); ++i) if(a[i] >= 'a' and a[i] <= 'z') a[i]-='a' - 'A'; return a; }
-string to_lower(string a){ for(int i=0; i < (int)a.size(); ++i) if(a[i] >= 'A' and a[i] <= 'Z') a[i]+='a' - 'A'; return a; }
-bool prime(ll a){ if(a == 1) return 0; for(int i=2; i <= round(sqrt(a)); ++i) if(a % i == 0) return 0; return 1; }
+string to_upper(string a) { for(int i = 0; i < (int)a.size(); ++i) if(a[i] >= 'a' and a[i] <= 'z') a[i] -= 'a' - 'A'; return a; }
+string to_lower(string a) { for(int i = 0; i < (int)a.size(); ++i) if(a[i] >= 'A' and a[i] <= 'Z') a[i] += 'a' - 'A'; return a; }
+bool prime(ll a) { if(a == 1) return 0; for(int i = 2; i <= round(sqrt(a)); ++i) if(a % i == 0) return 0; return 1; }
 
-void printYes(){ cout << "yes\n"; }
-void printNo(){ cout << "no\n"; }
+void printYes() { cout << "yes\n"; }
+void printNo() { cout << "no\n"; }
 
 template<typename T> // get greatest factor of number
-T greatestFactor(T n){ if(n <= 1) return n;for(T i=n / 2; i >= 1; --i){ if(n % i == 0){ return i; } }return -1; }
+T greatestFactor(T n) { if(n <= 1) return n; for(T i = n / 2; i >= 1; --i) { if(n % i == 0) { return i; } }return -1; }
 
 // sieve
-vector<bool> sieve(int n){ vector<bool> p(n+1,1); p[0]=p[1]=0;for(int i=2;i*i<=n;i++) if(p[i]) for(int j=i*i;j<=n;j+=i) p[j]=0;return p; }
+vector<bool> sieve(int n) { vector<bool> p(n + 1, 1); p[0] = p[1] = 0; for(int i = 2; i * i <= n; i++) if(p[i]) for(int j = i * i; j <= n; j += i) p[j] = 0; return p; }
 
 // DSU
-class DSU{
-public:vector<int>par,rank;DSU(int n){ par.resize(n);rank.resize(n);for(int i=0;i<n;i++)par[i]=i; }int find(int x){ return par[x]==x?x:par[x]=find(par[x]); }void unite(int x,int y){ int xp=find(x),yp=find(y);if(xp!=yp){ if(rank[xp]>rank[yp])par[yp]=xp;else if(rank[xp]<rank[yp])par[xp]=yp;else par[yp]=xp,rank[xp]++; } }
+class DSU {
+public:vector<int>par, rank; DSU(int n) { par.resize(n); rank.resize(n); for(int i = 0; i < n; i++)par[i] = i; }int find(int x) { return par[x] == x ? x : par[x] = find(par[x]); }void unite(int x, int y) { int xp = find(x), yp = find(y); if(xp != yp) { if(rank[xp] > rank[yp])par[yp] = xp; else if(rank[xp] < rank[yp])par[xp] = yp; else par[yp] = xp, rank[xp]++; } }
 };
 
 // main
-void solve(ll tc){
+void solve(ll tc) {
 
 }
 
 // driver
-int main(){
+int main() {
     fastio();
 
 // #ifndef ONLINE_JUDGE
-//     freopen("input.txt","r",stdin);
-// // freopen("output.txt","w",stdout);
+//     freopen("input.txt", "r", stdin);
+//     // freopen("output.txt", "w", stdout);
 // #endif
 
-    ll tc=1;
-    cin>>tc;
+    ll tc = 1;
+    cin >> tc;
 
-    for(ll i=1;i<=tc;i++){
+    for(ll i = 1; i <= tc; i++) {
         solve(i);
     }
 
